98032acf1855e7fff4661d0b331497076597b09c
Watcharawish Onsanit <watcharawish@Watcharawishs-MacBook-Pro.local>
2024-05-17 16:00:17 +0700

    Change files name

--- src/abv1.cpp
+++ src/abv1.cpp
@@ -1,71 +1,26 @@
 #include <Arduino.h>
 #include "definition.h"
 
-TMC2209Stepper driver(&SERIAL_PORT, R_SENSE, DRIVER_ADDRESS);
-
-extern void airbreak_up();
-extern void airbreak_down();
-
 void setup() {
-  Serial.begin(115200);
-  pinMode(TopSW, INPUT_PULLUP);
-  pinMode(BottomSW, INPUT_PULLUP);
-
-  pinMode(EN_PIN, OUTPUT);
-  pinMode(STEP_PIN, OUTPUT);
-  pinMode(DIR_PIN, OUTPUT);
-  digitalWrite(EN_PIN, LOW);      
-
-  SERIAL_PORT.begin(115200);      // HW UART drivers
-
-  driver.begin();                 //  SPI: Init CS pins and possible SW SPI pins
-  driver.toff(5);                 // Enables driver in software
-  driver.rms_current(0.961);        // Set motor RMS current
-  driver.microsteps(0);           // Set microsteps to 1/16th
-
-  driver.pwm_autoscale(true);     // Needed for stealthChop
+    Serial.begin(115200);
 }
 
 void loop() {
-    if (Serial.available() > 0) {
-        int incomingInt = Serial.parseInt();
-    
-        if (Serial.read() == '\n') {
-            Serial.print("Received valid integer: ");
-            ACCEL = incomingInt;
-            // Serial.println(ACCEL);
 
-            if (ACCEL == 1) {
-                driver.shaft(false);
-                airbreak_up();
-            }
-            else if (ACCEL == 0) {
-                driver.shaft(true);
-                airbreak_down();
-            }
-        }
-    }
-    if (digitalRead(TopSW) == LOW) {
+}
 
-        driver.shaft(false);
+double calculate_time(double velo, double accel) {
+    double v = 0.0; 
+    double time = (v - velo) / accel; 
+    return time;
+}
 
-        digitalWrite(STEP_PIN, HIGH);
-        delayMicroseconds(500);
-        digitalWrite(STEP_PIN, LOW);
-        delayMicroseconds(500);
-        //Serial.println("UP");
+double calculate_apogee(double time, double velo, double accel, double altitude) {
+    double initial_altitude = altitude; 
+    double u = 0.0; 
 
-    } else if (digitalRead(BottomSW) == LOW) {
-
-        driver.shaft(true);
-    
-        digitalWrite(STEP_PIN, HIGH);
-        delayMicroseconds(500);
-        digitalWrite(STEP_PIN, LOW);
-        delayMicroseconds(500);
-        //Serial.println("DOWN");
-        
-    }
+    double apogee = initial_altitude + (velo * time) + (0.5 * accel * time * time);
+    return apogee;
 }
 
 void airbreak_up() {
@@ -76,7 +31,6 @@ void airbreak_up() {
         delayMicroseconds(500);
         digitalWrite(STEP_PIN, LOW);
         delayMicroseconds(500);
-        //Serial.println("UP");
         Serial.println(i);
     }
 }
@@ -89,7 +43,8 @@ void airbreak_down() {
         delayMicroseconds(500);
         digitalWrite(STEP_PIN, LOW);
         delayMicroseconds(500);
-        //Serial.println("DOWN");
         Serial.println(i);
     }
 }
+
+
